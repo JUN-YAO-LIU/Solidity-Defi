78a1fbd73c9886db312e61857bf3aae0be0638d8
JUN-YAO-LIU <momomomomo7077@gmail.com>
2023-05-04 16:27:03 +0800

    uniswap

--- Defi/Uniswap/swapv2.sol
+++ Defi/Uniswap/swapv2.sol
@@ -2,14 +2,20 @@
 pragma solidity ^0.8.17;
 
 contract UniswapV2SwapExamples {
+
+    // 合約地址
     address private constant UNISWAP_V2_ROUTER =
         0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
+    // 代幣地址
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
+    // 取得合約
     IUniswapV2Router private router = IUniswapV2Router(UNISWAP_V2_ROUTER);
+
+    // 創建代幣
     IERC20 private weth = IERC20(WETH);
     IERC20 private dai = IERC20(DAI);
 
