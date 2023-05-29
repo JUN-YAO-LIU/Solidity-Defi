665582b8a588950882af7a37c842b1414c886304
jim.liu <jim.liu@numiner.io>
2023-05-29 16:22:12 +0800

    d

--- contracts/Update/Wrong.sol
+++ contracts/Update/Wrong.sol
@@ -14,9 +14,9 @@ contract CounterV1 {
 }
 
 contract CounterV2 {
-    uint public count;
     address public implementation;
     address public admin;
+    uint public count;
 
     function inc() external {
         count += 1;
@@ -30,6 +30,8 @@ contract CounterV2 {
 contract BuggyProxy {
     address public implementation;
     address public admin;
+    // 如果此合約沒有count，我們就無法調用delegateCall方法更新值。
+    // uint public count;
 
     constructor() {
         admin = msg.sender;
@@ -46,6 +48,68 @@ contract BuggyProxy {
 
     receive() external payable {
         _delegate();
+    }
+
+    function upgradeTo(address _implementation) external {
+        require(msg.sender == admin, "not authorized");
+        implementation = _implementation;
+    }
+}
+
+contract ResolveBuggyProxy {
+    address public implementation;
+    address public admin;
+
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function _delegate(address _implementation) internal virtual {
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+
+            // calldatacopy(t, f, s) - copy s bytes from calldata at position f to mem at position t
+            // calldatasize() - size of call data in bytes
+            calldatacopy(0, 0, calldatasize())
+
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+
+            // delegatecall(g, a, in, insize, out, outsize) -
+            // - call contract at address a
+            // - with input mem[in…(in+insize))
+            // - providing g gas
+            // - and output area mem[out…(out+outsize))
+            // - returning 0 on error (eg. out of gas) and 1 on success
+            let result := delegatecall(gas(), _implementation, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            // returndatacopy(t, f, s) - copy s bytes from returndata at position f to mem at position t
+            // returndatasize() - size of the last returndata
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 {
+                // revert(p, s) - end execution, revert state changes, return data mem[p…(p+s))
+                revert(0, returndatasize())
+            }
+            default {
+                // return(p, s) - end execution, return data mem[p…(p+s))
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    fallback() external payable {
+        _delegate(implementation);
+    }
+
+    receive() external payable {
+        _delegate(implementation);
     }
 
     function upgradeTo(address _implementation) external {
